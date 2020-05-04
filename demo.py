4fadce0f8f2c318aab2c4c842acd1b980a02a348
Qingyuan Ma <qm28@georgetown.edu>
2020-05-04 19:51:03 -0400

    practice the two types of  string interpolation

--- demo.py
+++ demo.py
@@ -5,6 +5,8 @@ connection = psycopg2.connect("dbname=example user=qingyuan password=MAzhang1997
 # cursor is the interface: queing up and making transactions
 cursor = connection.cursor()
 
+cursor.execute('DROP TABLE IF EXISTS table2;')
+
 cursor.execute('''
     CREATE TABLE table2 (
         id INTEGER PRIMARY KEY,
@@ -12,7 +14,16 @@ cursor.execute('''
     );
 ''')
 
-cursor.execute('INSERT INTO table2 (id, complete) VALUES (1, true);')
+cursor.execute('INSERT INTO table2 (id, complete) VALUES (%s, %s);', (1, True))
+
+cursor.execute('''
+INSERT INTO table2 (id, complete) VALUES (%(id)s, %(complete)s);
+''',
+{
+    "id": 2,
+    "complete": False
+}
+)
 
 # cursor.execute will not execute the commands in sql immediately, it need to commit first,
 # so here we commit both as a whole unit
